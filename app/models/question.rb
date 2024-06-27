68ae2bfef11acc906b59d55af3f04ba68a903279
Francisco Macias <panchojmacias@gmail.com>
2024-06-27 09:03:07 -0300

    flair

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   validates :content, :topic, presence: true
+
+  TOPIC = ["Python", "C", "Flask", "Algorithms", "Memory"].sort
+
+  validates :topic, inclusion: {in: TOPIC}
 end

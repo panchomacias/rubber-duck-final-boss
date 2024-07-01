b43ef1ff46e70f096d44e519f415dfe1cbcc2d71
Francisco Macias <panchojmacias@gmail.com>
2024-07-01 17:01:10 -0300

    gemfile

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "font-awesome-sass", "~> 6.5.1"
+
+gem "simple_form-tailwind"
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,14 +5,14 @@ class Question < ApplicationRecord
 
   def assign_color
     topic_colors = {
-      "Python" => "#FF5733",
-      "C" => "#33FF57",
-      "Flask" => "#3357FF",
-      "Memory" => "#FF33A5",
-      "Algorithms" => "#FF5733",
+      'Python' => '#FF5733',
+      'C' => '#33FF57',
+      'Flask' => '#3357FF',
+      'Memory' => '#FF33A5',
+      'Algorithms' => '#FF5733'
       # Añade más tópicos y colores aquí
     }
 
-    self.color = topic_colors[self.topic] || "#000000" # Color por defecto si no se encuentra el tópico
+    self.color = topic_colors[topic] || '#000000' # Color por defecto si no se encuentra el tópico
   end
 end
